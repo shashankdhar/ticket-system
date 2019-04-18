1ad4303519254548fc155360e27e450f89459369
shashankdhar75 <shashankdhar75@gmail.com>
2019-04-17 21:29:40 -0400

    Modal UI

--- src/components/modal-ticket.vue
+++ src/components/modal-ticket.vue
@@ -17,56 +17,56 @@
       <div class="modal modal-ticket" role="dialog"  aria-labelledby="modalTitle" aria-describedby="modalDescription">
         <header class="modal-header" id="modalTitle">
           <slot name="header">
-            <h5>Ticket Details</h5>
+            <h5>Ticket details</h5>
             <button type="button" class="btn-close"  @click="close" aria-label="Close modal">x</button>
           </slot>
         </header>
         <section class="modal-body" id="modalDescription">
           <slot name="body">
-            <table class="table table-bordered table-sm">
+            <table class="table table-sm">
             <tbody>
               <tr>
-                <th scope="row">Ticket No</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Ticket No</th>
                 <td>{{ ticket.ticket }}</td>
               </tr>
               <tr>
-                <th scope="row">Requestor Id</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Requestor Id</th>
                 <td>{{ ticket.Requestor }}</td>
               </tr>
               <tr>
-                <th scope="row">Requestor Seniority</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Requestor Seniority</th>
                 <td>{{ ticket.RequestorSeniority }}</td>
               </tr>
               <tr>
-                <th scope="row">IT Owner</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">IT Owner</th>
                 <td>{{ ticket.ITOwner }}</td>
               </tr>
               <tr>
-                <th scope="row">Filed Against</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Filed Against</th>
                 <td>{{ ticket.FiledAgainst }}</td>
               </tr>
               <tr>
-                <th scope="row">Ticket Type</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Ticket Type</th>
                 <td>{{ ticket.TicketType }}</td>
               </tr>
               <tr>
-                <th scope="row">Severity</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Severity</th>
                 <td>{{ ticket.Severity }}</td>
               </tr>
               <tr>
-                <th scope="row">Priority</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Priority</th>
                 <td>{{ ticket.Priority }}</td>
               </tr>
               <tr>
-                <th scope="row">daysOpen</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">No. of days</th>
                 <td>{{ ticket.daysOpen }}</td>
               </tr>
               <tr>
-                <th scope="row">Satisfaction</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Satisfaction rating</th>
                 <td>{{ ticket.Satisfaction }}</td>
               </tr>
               <tr>
-                <th scope="row">Ticket Creation Date</th>
+                <th class="text-left px-2 font-weight-bold" scope="row">Ticket Creation Date</th>
                 <td>{{ ticket["Ticket Creation Date"] }}</td>
               </tr>
             </tbody>
@@ -84,67 +84,5 @@
   </transition>
 </template>
 <style>
-  .modal-backdrop.modal-backdrop-ticket {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .modal.modal-ticket {
-    background: #FFFFFF;
-    box-shadow: 2px 2px 20px 1px;
-    overflow-x: auto;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .modal-ticket .modal-header,
-  .modal-ticket .modal-footer {
-    padding: 5px 15px;
-    display: flex;
-  }
-
-  .modal-ticket .modal-header {
-    border-bottom: 1px solid #eeeeee;
-    color: #4AAE9B;
-    justify-content: space-between;
-  }
-
-  .modal-ticket .modal-header h5 {
-      margin-top: 0.5rem;
-  }
-
-  .modal-ticket .modal-footer {
-    border-top: 1px solid #eeeeee;
-    justify-content: flex-end;
-  }
-
-  .modal-ticket .modal-body {
-    position: relative;
-    padding: 20px 10px;
-  }
-
-  .modal-ticket .btn-close {
-    border: none;
-    font-size: 20px;
-    /*padding: 20px;*/
-    cursor: pointer;
-    font-weight: bold;
-    color: #4AAE9B;
-    background: transparent;
-  }
-
-  .modal-ticket .btn-green {
-    color: white;
-    background: #4AAE9B;
-    border: 1px solid #4AAE9B;
-    border-radius: 2px;
-    padding: 5px;
-  }
+    @import '../assets/css/modal.css';
 </style>
